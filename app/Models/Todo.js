4b8e85fede23c53b0950e7f0c6b78336644ccc58
MatthewHufft <matthufft777@gmail.com>
2020-09-06 22:00:40 -0600

    all requirements met, fully functioning app

--- app/Models/Todo.js
+++ app/Models/Todo.js
@@ -4,21 +4,28 @@ export default class Todo {
     this.completed = completed
     this.user = user
     this.description = description || "no description"
-    this.checked = ""
-
-    if (this.completed){
-      this.checked = "checked"
-    } else {this.checked = ""}
   }
 
   get Template () {
-    return `
+
+    if(this.completed) {
+      return `
+        <li class="d-flex justify-content-between my-1 mx-1 darker-bg no-select"> 
+          <div class="custom-control custom-checkbox" onclick="app.todoController.toggleTodoStatus('${this.id}')">
+            <input type="checkbox" class="custom-control-input" id="${this.id}"  checked>
+            <label class="custom-control-label" for="${this.id}">${this.description}</label>
+          </div>
+          <span> <i class="fa fa-times-circle-o" aria-hidden="true" onclick="app.todoController.removeTodo('${this.id}')"></i> </span>
+        </li> `
+    } else {
+      return `
       <li class="d-flex justify-content-between my-1 mx-1 darker-bg no-select"> 
         <div class="custom-control custom-checkbox" onclick="app.todoController.toggleTodoStatus('${this.id}')">
-          <input type="checkbox" class="custom-control-input" id="${this.id}"  ${this.checked}>
+          <input type="checkbox" class="custom-control-input" id="${this.id}">
           <label class="custom-control-label" for="${this.id}">${this.description}</label>
         </div>
         <span> <i class="fa fa-times-circle-o" aria-hidden="true" onclick="app.todoController.removeTodo('${this.id}')"></i> </span>
       </li> `
+    }
   }
 } 
